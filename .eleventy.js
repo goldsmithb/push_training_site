cb059736be2347886aea6e31d76e4f08f68e7565
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2025-05-11 13:06:56 -0400

    11ty img plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
-    
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
