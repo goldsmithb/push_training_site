0950972dcec7b62da09d8699a901d8e5f9126d6d
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2025-06-29 12:26:10 -0400

    add page content

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+// Look into: Inline Minified CSS Tips (see 11ty docs)
+
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default async function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("global.css");
+    eleventyConfig.addPassthroughCopy("navbar.css");
+    eleventyConfig.addPassthroughCopy("footer.css");
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPlugin(eleventyImageTransformPlugin);
     return {
