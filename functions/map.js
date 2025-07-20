2c4d38a9bbfb36b0b07af431abbcfb9a74ac4d9d
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2025-07-20 10:02:53 -0400

    use function to get map url

--- functions/map.js
+++ functions/map.js
@@ -1,29 +1,25 @@
-// export async function onRequest(context) {
-//   const { GM_API_KEY } = context.env;
-
-// //   const params = new URL(context.request.url).searchParams;
-// //   const place = params.get("q");
-
-// //   if (!place) {
-// //     return new Response("Missing 'q' query param", { status: 400 });
-// //   }
-
-//   const q = "50+S+Buckhout+St+#+304,Irvington,NY+10533,United+States"
-
-//   // const embedUrl = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(place)}`;
-
-//   const embedURL = `https://www.google.com/maps/embed/v1/place?key=${GM_API_KEY}&q=${q}`
-
-//   return embedURL
-
-//   // return new Response(null, {
-//   //   status: 302,
-//   //   headers: {
-//   //     Location: embedUrl
-//   //   }
-//   // });
-// }
-
 export async function onRequest(context) {
-  return new Response("Hello from map function!");
+  const { GM_API_KEY } = context.env;
+
+//   const params = new URL(context.request.url).searchParams;
+//   const place = params.get("q");
+
+//   if (!place) {
+//     return new Response("Missing 'q' query param", { status: 400 });
+//   }
+
+  const q = "50+S+Buckhout+St+#+304,Irvington,NY+10533,United+States"
+
+  // const embedUrl = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(place)}`;
+
+  const embedURL = `https://www.google.com/maps/embed/v1/place?key=${GM_API_KEY}&q=${q}`
+
+  return embedURL
+
+  // return new Response(null, {
+  //   status: 302,
+  //   headers: {
+  //     Location: embedUrl
+  //   }
+  // });
 }
