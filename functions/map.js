8ab7f475d65abb634270bbc71be80253e4c52c83
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2025-07-22 01:18:53 -0400

    location page

--- functions/map.js
+++ functions/map.js
@@ -1,16 +1,7 @@
 export async function onRequest(context) {
   const { GM_API_KEY } = context.env;
 
-//   const params = new URL(context.request.url).searchParams;
-//   const place = params.get("q");
-
-//   if (!place) {
-//     return new Response("Missing 'q' query param", { status: 400 });
-//   }
-
   const q = "50+S+Buckhout+St+#+304,Irvington,NY+10533,United+States"
-
-  // const embedUrl = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAPS_API_KEY}&q=${encodeURIComponent(place)}`;
 
   const embedURL = `https://www.google.com/maps/embed/v1/place?key=${GM_API_KEY}&q=${q}`
 
@@ -21,7 +12,6 @@ export async function onRequest(context) {
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade">
     </iframe>`
-  // return embedURL
   
   return new Response(html, {
     status: 200,
@@ -29,11 +19,4 @@ export async function onRequest(context) {
       "Content-Type": "text/plain"
     }
   });
-
-  // return new Response(null, {
-  //   status: 302,
-  //   headers: {
-  //     Location: embedUrl
-  //   }
-  // });
 }
